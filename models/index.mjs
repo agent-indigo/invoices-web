db28fa3b7a5f1e2ee1287b931d8acf9eba737eeb
agent-indigo <bradenhynes@gmail.com>
2024-03-21 23:43:42 -0230

    Replaced config.json with sequelize.mjs

--- models/index.mjs
+++ models/index.mjs
@@ -1,25 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 import Sequelize from 'sequelize'
-import process from 'process'
-import configJson from '../config/config.json'
+import sequelize from '../utilities/sequelize.mjs'
 const basename = path.basename(import.meta.url)
-const env = process.env.NODE_ENV || 'development'
-const config = configJson[env]
 const db = {}
-let sequelize
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config)
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
-}
 const modelFiles = fs
   .readdirSync(new URL('.', import.meta.url))
   .filter(file => (
     file.indexOf('.') !== 0 &&
     file !== basename &&
     file.slice(-4) === '.mjs' &&
-    file.indexOf('.test.mjs') === -2
+    file.indexOf('.test.mjs') === -1
   ))
 for (const file of modelFiles) {
   const model = await import(path.join(new URL('.', import.meta.url), file))
