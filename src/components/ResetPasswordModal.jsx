a4df46d4b32fded712a39bb6430dcf7bc5c46f8e
agent-indigo <bradenhynes@gmail.com>
2024-03-25 00:40:23 -0230

    Built password reset modal

---
+++ src/components/ResetPasswordModal.jsx
@@ -0,0 +1,72 @@
+import {useState} from 'react'
+import {Form, Button, Modal} from 'react-bootstrap'
+import {FaKey, FaTimes, FaCheck, FaEdit} from 'react-icons/fa'
+import FormContainer from '../components/FormContainer'
+import Loader from '../components/Loader'
+import {useResetPasswordMutation} from '../slices/usersApiSlice'
+import {toast} from 'react-toastify'
+const ResetPasswordModal = pk => {
+    const [newPassword, setNewPassword] = useState('')
+    const [confirmNewPassword, setConfirmNewPassword] = useState('')
+    const [resetPassword, {isLoading}] = useResetPasswordMutation()
+    const submitHandler = async event => {
+      event.preventDefault()
+      try {
+        const response = await resetPassword({pk, newPassword, confirmNewPassword}).unwrap()
+        Modal.close()
+        toast.success(response.message)
+      } catch (error) {
+        toast.error(error?.data?.message || error.error)
+      }
+    }
+  return (
+    <Modal>
+        <FormContainer>
+            <h1><FaKey/> Reset password</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='newPassword' className='my-3'>
+                <Form.Label><FaEdit/> New password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Enter new password'
+                    value={newPassword}
+                    onChange={event => setNewPassword(event.target.value)}
+                    autoFocus
+                ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='confirmNewPassword' className='my-3'>
+                <Form.Label><FaCheck/> Confirm new password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Confirm new password'
+                    value={confirmNewPassword}
+                    onChange={event => setConfirmNewPassword(event.target.value)}
+                    onKeyDown={event => {
+                    if (event.key === 'Enter') {
+                        event.preventDefault();
+                        submitHandler(event);
+                    }
+                    }}
+                ></Form.Control>
+                </Form.Group>
+                <Button
+                    type='submit'
+                    variant='success'
+                    className='m-auto p-auto text-white'
+                    disabled={isLoading}
+                ><FaCheck/> Reset</Button>
+                {' '}
+                <Button
+                    type='button'
+                    variant='danger'
+                    className='m-auto p-auto text-white'
+                    disabled={isLoading}
+                    onClick={Modal.close()}
+                ><FaTimes/> Cancel</Button>
+                {isLoading && <Loader/>}
+            </Form>
+        </FormContainer>
+    </Modal>
+  )
+}
+export default ResetPasswordModal
