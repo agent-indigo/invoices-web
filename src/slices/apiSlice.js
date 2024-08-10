896e4466b309ae74975b8cf5fd9427f47e8a7ea4
agent-indigo <bradenhynes@gmail.com>
2024-08-10 15:10:59 -0230

    Vertical realignment

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,17 +1,20 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import {
+  createApi,
+  fetchBaseQuery
+} from '@reduxjs/toolkit/query/react'
 import {BASE_URL} from '../urls'
 const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
     prepareHeaders: (headers, {getState}) => {
       const token = getState()?.authentication?.user?.token
-      if (token) headers.set('Authorization', `Bearer ${token}`)
+      if (token) headers.set(
+        'Authorization',
+        `Bearer ${token}`
+      )
     },
   }),
-  tagTypes: [
-    'user',
-    'invoice'
-  ],
+  tagTypes: ['user', 'invoice'],
   endpoints: builder => ({})
 })
 export default apiSlice
